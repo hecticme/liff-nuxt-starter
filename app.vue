6f89e39f45d30dcdabb3f4b7c1afdee2830744ee
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-08 11:16:06 +0700

    chore: set page's title and meta description

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'LIFF + Nuxt',
+  meta: [
+    {
+      name: 'description',
+      content: 'A LIFF web app built with Nuxt.',
+    },
+  ],
+})
+</script>
+
 <template>
   <div>
     app
