5a796ad3db6a6ea93edc27b0f5aad65839bdbe77
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-08 15:13:37 +0700

    chore: set liffId through runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2024-07-08',
+  runtimeConfig: {
+    public: {
+      liffId: process.env.NUXT_LIFF_ID,
+    },
+  },
 })
