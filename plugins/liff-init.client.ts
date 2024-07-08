e18b093cc30d2f29ea3b3abf26d33f3908a526d4
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-08 15:14:49 +0700

    fix: use `liffId` in LIFF init plugin

--- plugins/liff-init.client.ts
+++ plugins/liff-init.client.ts
@@ -1,13 +1,15 @@
 import liff from '@line/liff'
 
 export default defineNuxtPlugin(() => {
-  if (!process.env.LIFF_ID) {
+  const config = useRuntimeConfig()
+
+  if (!config.public.liffId) {
     console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.')
 
     return
   }
 
-  const initResult = liff.init({ liffId: process.env.LIFF_ID })
+  const initResult = liff.init({ liffId: config.public.liffId })
     .then(() => {
       console.log('liff.init() done')
     })
