5ab0e3c671d543b082bfd442fd92c0eaa38b8277
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-09 10:20:06 +0700

    chore: configure Nuxt to use main stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: '2024-07-08',
+  css: [
+    '~/assets/css/main.css',
+  ],
   runtimeConfig: {
     public: {
       liffId: process.env.LIFF_ID,
